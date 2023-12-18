74a5fcd020461c3371424e9fe97e22ff9df16c2f
Astarcys <stuanubhab14196@kvschennairegion.in>
2023-12-18 21:14:41 +0530

    Fixed the volume function

--- soundapp.py
+++ soundapp.py
@@ -30,7 +30,8 @@ while True:
     elif n.lower()=='v':
         print("Current volume:",mixer.music.get_volume()*100,'%')
         v=(float(input("Enter Volume: ")))/100
-        mixer.music.set_volume(v)
+        if mixer.music.get_busy()==True:
+            mixer.music.set_volume(v)
         print("New volume:",v*100,'%')
     elif n.lower()=='h':
         print("\nKindly ignore the quotes and enter the characters only\n1. Enter 'P' to play a song\n2. Enter 'L' to view the list of songs in the songs folder\n3. Enter 'PP' to pause the current song\n4. Enter 'r' to resume\n5. Enter 'S' to stop playing the current song\n6. Enter 'V' to change the Volume\n7. Enter 'Q' to exit the app")
