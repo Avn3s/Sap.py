e43a932f0a9b5fb2d5ea1ec8df6f6602daa7e1ad
Avn3s <154056573+Avn3s@users.noreply.github.com>
2023-12-22 17:05:39 +0530

    added functionality

--- pygameinst.py
+++ pygameinst.py
@@ -1,6 +1,10 @@
 from os import system
 from time import sleep
+system("py -m pip install --upgrade pip")
+print("pip successfully updated!")
 system("python -m pip install pygame")
-print("\nPygame Successfully installed!\nThis window will close in 5 sec.")
+print("\nPygame Successfully installed!")
+system("python -m pip install --upgrade pygame")
+print("Pygame successfully updated!")
 sleep(5)
 exit()
