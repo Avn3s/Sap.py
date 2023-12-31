34bfc2a7b50ee6ce100341c31b75f0a7e9c8e391
Astarcys <stuanubhab14196@kvschennairegion.in>
2023-12-31 15:26:14 +0530

    fixed minor bug

--- requirementinstaller.py
+++ requirementinstaller.py
@@ -1,6 +1,6 @@
 from os import system
 from time import sleep
-system("py -m pip install --upgrade pip")
+system("python -m pip install --upgrade pip")
 print("pip successfully updated!")
 system("python -m pip install pygame")
 print("\nPygame Successfully installed!")
