ab47342c259e656e82b67e7172685537c85d926e
Astarcys <stuanubhab14196@kvschennairegion.in>
2024-02-06 16:25:46 +0530

    installer bugfix

--- requirementinstaller.py
+++ requirementinstaller.py
@@ -4,11 +4,11 @@ if name=="nt":
     x="py"
 else:
     x="python3"
-system(x+"-m pip install --upgrade pip")
+system(x+" -m pip install --upgrade pip")
 print("pip successfully updated!")
-system(x+"-m pip install pygame")
+system(x+" -m pip install pygame")
 print("\nPygame Successfully installed!")
-system(x+"-m pip install --upgrade pygame")
+system(x+" -m pip install --upgrade pygame")
 print("Pygame successfully updated!")
 sleep(5)
 exit()
