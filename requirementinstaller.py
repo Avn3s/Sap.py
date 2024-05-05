73d3a0d9f7ea7e5b80ce8220eb603433b9dffbd0
Avn3s <astarcys7@gmail.com>
2024-05-05 16:07:37 +0530

    Made the code prettier

--- requirementinstaller.py
+++ requirementinstaller.py
@@ -1,14 +1,15 @@
-from os import system,name
+from os import system, name
 from time import sleep
-if name=="nt":
-    x="py"
+
+if name == "nt":
+    x = "py"
 else:
-    x="python3"
-system(x+" -m pip install --upgrade pip")
+    x = "python3"
+system(x + " -m pip install --upgrade pip")
 print("pip successfully updated!")
-system(x+" -m pip install pygame")
+system(x + " -m pip install pygame")
 print("\nPygame Successfully installed!")
-system(x+" -m pip install --upgrade pygame")
+system(x + " -m pip install --upgrade pygame")
 print("Pygame successfully updated!")
 sleep(5)
 exit()
