559e16c0d1715fcfb581a6668cf68d3ef8dcf2bc
Avn3s <astarcys7@gmail.com>
2024-07-13 18:46:13 +0530

    trimmed

--- something.py
+++
@@ -1,9 +0,0 @@
-import json
-
-dict1 = {
-    "name": "kira",
-    "age": 20,
-    "status": "dead",
-}
-with open("something.json", "w") as dumpfile:
-    json.dump(dict1, dumpfile, indent=4)
--- something_else.py
+++
@@ -1,6 +0,0 @@
-import json
-
-with open("something_else.json", "r") as file:
-    data = json.load(file)
-
-print(data['BOB'])
